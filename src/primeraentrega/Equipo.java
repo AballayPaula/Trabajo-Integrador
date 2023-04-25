209e1c959d92f24e8305323d76b76e5baf0fc10b
AballayPaula <aballaypaula02@gmail.com>
2023-04-25 10:00:45 -0300

    Commit

---
+++ src/primeraentrega/Equipo.java
@@ -0,0 +1,5 @@
+package primeraentrega;
+
+public class Equipo {
+
+}
---
+++ src/primeraentrega/MainTP.java
@@ -0,0 +1,10 @@
+package primeraentrega;
+
+public class MainTP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
